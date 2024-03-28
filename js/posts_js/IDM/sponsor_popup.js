61bc2f719aba503fd95a361b4fe874f61254aad3
NNNH <77568569+yhgzs-111@users.noreply.github.com>
2024-03-28 12:22:58 +0800

    update

--- js/posts_js/IDM/sponsor_popup.js
+++ js/posts_js/IDM/sponsor_popup.js
@@ -6,7 +6,7 @@ function openSponsorPopup() {
     var left = (window.innerWidth - width) / 2;
     var top = (window.innerHeight - height) / 2;
     
-    popup = window.open('../赞助码.png', 'sponsorPopup', 'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
+    popup = window.open('../img/赞助码.png', 'sponsorPopup', 'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
     
     popup.focus();
 }
